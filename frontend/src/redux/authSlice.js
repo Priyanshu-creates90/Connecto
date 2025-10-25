c2d94b313381228d03c02456f0dcd7d998c919dc
Priyanshu-creates90 <priyanshu902613@gmail.com>
2025-10-25 23:54:15 +0530

    frontend 1.2

---
+++ frontend/src/redux/authSlice.js
@@ -0,0 +1,21 @@
+import SuggestedUsers from "@/components/SuggestedUsers";
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const authSlice=createSlice({
+    name:"auth",
+    initialState:{
+        user:null,
+        suggestedUsers:[]
+    },
+    reducers:{
+        setAuthUser:(state,action)=>{
+            state.user=action.payload;
+        },
+        setSuggestedUsers:(state,action) =>{
+            state.suggestedUsers=action.payload;
+        }
+    }
+});
+export const {setAuthUser,setSuggestedUsers}=authSlice.actions;
+export default authSlice.reducer;
