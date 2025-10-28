f84b8cd0bc80007a48a1ca981596d6132db1461b
Priyanshu-creates90 <priyanshu902613@gmail.com>
2025-10-29 00:09:29 +0530

    frontend 1.2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,13 @@ import MainLayout from './components/MainLayout'
 
 import Profile from './components/Profile'
 import Home from './components/Home'
-
+import EditProfile from './components/EditProfile'
+import ChatPage from './components/ChatPage'
+import {io} from 'socket.io-client';
+import { useDispatch, useSelector } from 'react-redux'
+import { useEffect } from 'react'
+import { setSocket } from './redux/socketSlice'
+import { setOnlineUsers } from './redux/chatSlice'
 
 const browserRouter=createBrowserRouter([
   {
@@ -20,6 +26,13 @@ const browserRouter=createBrowserRouter([
       {
         path:'/profile/:id',
         element:<Profile />
+      },
+      {
+        path:'/account/edit',
+        element:<EditProfile />
+      }, {
+        path:'/chat',
+        element:<ChatPage />
       },
     ]
   },
@@ -35,7 +48,32 @@ const browserRouter=createBrowserRouter([
 
 function App() {
 
-  return (
+  const {user}=useSelector(store=> store.auth);
+  const {socket}=useSelector(store=>store.socketio);
+  const dispatch = useDispatch ();
+  useEffect(() => {
+       if (user) {
+        const socketio= io('http://localhost:8000',{
+          query:{
+            userId:user?._id
+          },
+          transports:['websocket']
+        });
+        dispatch(setSocket(socketio));
+        //listen all the events
+        socketio.on('getOnlineUsers',(onlineUsers)=>{
+          dispatch(setOnlineUsers(onlineUsers));
+        });
+          return ()=>{
+            socketio.close();
+            dispatch(setSocket(null));
+          }
+       } else if(socket){
+          socket?.close();
+        dispatch(setSocket(null));
+       }
+  },[user,dispatch]);
+    return (
     <>
       <RouterProvider router={browserRouter}/>
     </>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import ReactDom from "react-dom/client"
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+
+
 import './index.css'
 import App from './App.jsx'
-import { Toaster } from 'sonner'
+import { Toaster } from './components/ui/sonner.jsx'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
 import { PersistGate } from 'redux-persist/integration/react'
 import { persistStore } from 'redux-persist'
 
 let persistor = persistStore(store)
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDom.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor} > 
     <App />
     <Toaster />
     </PersistGate> 
     </Provider>
-  </StrictMode>,
+  </React.StrictMode>,
 )
