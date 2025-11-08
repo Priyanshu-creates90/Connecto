6dd5cf3c297a43169116eaa79a440300137cbcf5
Priyanshu-creates90 <priyanshu902613@gmail.com>
2025-11-09 02:00:34 +0530

    frontend 1.8

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,89 +1,91 @@
-import { Button } from './components/ui/button'
-import Singup from './components/Singup.jsx'
-import Login from './components/Login'
-import {createBrowserRouter,RouterProvider } from 'react-router-dom'
-import MainLayout from './components/MainLayout'
+import { Button } from "./components/ui/button";
+import Singup from "./components/Singup.jsx";
+import Login from "./components/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainLayout from "./components/MainLayout";
 
-import Profile from './components/Profile'
-import Home from './components/Home'
-import EditProfile from './components/EditProfile'
-import ChatPage from './components/ChatPage'
-import {io} from 'socket.io-client';
-import { useDispatch, useSelector } from 'react-redux'
-import { useEffect } from 'react'
-import { setSocket } from './redux/socketSlice'
-import { setOnlineUsers } from './redux/chatSlice'
-import { setLikeNotification } from './redux/rtnSlice'
+import Profile from "./components/Profile";
+import Home from "./components/Home";
+import EditProfile from "./components/EditProfile";
+import ChatPage from "./components/ChatPage";
+import { io } from "socket.io-client";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { setSocket } from "./redux/socketSlice";
+import { setOnlineUsers } from "./redux/chatSlice";
+import { setLikeNotification } from "./redux/rtnSlice";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
-const browserRouter=createBrowserRouter([
+const browserRouter = createBrowserRouter([
   {
     path: "/",
-    element:<MainLayout />,
-    children:[
+    element: <ProtectedRoutes> <MainLayout /> </ProtectedRoutes> ,
+    children: [
       {
-        path:'/',
-        element:<Home/>
+        path: "/",
+        element: <ProtectedRoutes> <Home /> </ProtectedRoutes> ,
       },
       {
-        path:'/profile/:id',
-        element:<Profile />
+        path: "/profile/:id",
+        element: <ProtectedRoutes> <Profile /> </ProtectedRoutes> ,
       },
       {
-        path:'/account/edit',
-        element:<EditProfile />
-      }, {
-        path:'/chat',
-        element:<ChatPage />
+        path: "/account/edit",
+        element: <ProtectedRoutes> <EditProfile /> </ProtectedRoutes>,
       },
-    ]
+      {
+        path: "/chat",
+        element: <ProtectedRoutes> <ChatPage /> </ProtectedRoutes>,
+      },
+    ],
   },
-      {
-        path:'/login',
-        element: <Login />
-      },
-      {
-        path:'/singup',
-        element: <Singup />
-  }
-])
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/singup",
+    element: <Singup />,
+  },
+]);
 
 function App() {
-
-  const {user}=useSelector(store=> store.auth);
-  const {socket}=useSelector(store=>store.socketio);
-  const dispatch = useDispatch ();
+  const { user } = useSelector((store) => store.auth);
+  const { socket } = useSelector((store) => store.socketio);
+  const dispatch = useDispatch();
   useEffect(() => {
-       if (user) {
-        const socketio= io('http://localhost:8000',{
-          query:{ //query because we have to send user id in the backend
-            userId:user?._id
-          }, 
-          transports:['websocket']  //many unsecessary api calling  in Network to stop that we use 
-        });
-        dispatch(setSocket(socketio));
-        //listen all the events
-        socketio.on('getOnlineUsers',(onlineUsers)=>{
-          dispatch(setOnlineUsers(onlineUsers));
-        });
-            //notification
-        socketio.on("notification",(notification)=>{
-          dispatch (setLikeNotification(notification));
-          
-        })  ;
-          return ()=>{  //cleanup ,when user left the page it show ofline
-            socketio.close();
-            dispatch(setSocket(null));
-          }
-       } else if(socket){
-          socket?.close();
+    if (user) {
+      const socketio = io("http://localhost:8000", {
+        query: {
+          //query because we have to send user id in the backend
+          userId: user?._id,
+        },
+        transports: ["websocket"], //many unsecessary api calling  in Network to stop that we use
+      });
+      dispatch(setSocket(socketio));
+      //listen all the events
+      socketio.on("getOnlineUsers", (onlineUsers) => {
+        dispatch(setOnlineUsers(onlineUsers));
+      });
+      //notification
+      socketio.on("notification", (notification) => {
+        dispatch(setLikeNotification(notification));
+      });
+      return () => {
+        //cleanup ,when user left the page it show ofline
+        socketio.close();
         dispatch(setSocket(null));
-       }
-  },[user , dispatch ]);
-    return (
+      };
+    } else if (socket) {
+      socket?.close();
+      dispatch(setSocket(null));
+    }
+  }, [user, dispatch]);
+  return (
     <>
-      <RouterProvider router={browserRouter}/>
+      <RouterProvider router={browserRouter} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
