a1f63372b4c9a6250de862e64937fcbada5088ed
Priyanshu-creates90 <priyanshu902613@gmail.com>
2025-11-15 04:12:27 +0530

    updated

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -36,7 +36,7 @@ const Post = ({ post }) => {
     try {
       const action = liked ? "dislike" : "like";
       const res = await axios.get(
-        `https://connecto-1-psxd.onrender.com/api/v1/post/${post._id}/${action}`,
+        `http://localhost:8000/api/v1/post/${post._id}/${action}`,
         { withCredentials: true }
       );
       if (res.data.success) {
@@ -67,7 +67,7 @@ const Post = ({ post }) => {
   const commentHandler = async () => {
     try {
       const res = await axios.post(
-        `https://connecto-1-psxd.onrender.com/api/v1/post/${post._id}/comment`,
+        `http://localhost:8000/api/v1/post/${post._id}/comment`,
         { text },
         {
           headers: {
@@ -97,7 +97,7 @@ const Post = ({ post }) => {
   const deletePostHandler = async () => {
     try {
       const res = await axios.delete(
-        `https://connecto-1-psxd.onrender.com/api/v1/post/delete/${post?._id}`,
+        `http://localhost:8000/api/v1/post/delete/${post?._id}`,
         { withCredentials: true }
       );
       if (res.data.success) {
@@ -116,7 +116,7 @@ const Post = ({ post }) => {
   const bookmarkHandler = async () => {
     try {
       const res = await axios.get(
-        `https://connecto-1-psxd.onrender.com/api/v1/post/${post?._id}/bookmark`,
+        `http://localhost:8000/api/v1/post/${post?._id}/bookmark`,
         { withCredentials: true }
       );
       if (res.data.success) {
